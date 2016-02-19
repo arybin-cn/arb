53f305bb7effbdfb5de462bdb79e37c8387597cd
arybin <arybin@163.com>
2016-02-19 21:26:05 +0800

    remove arb_init and add the enhanced method "arb_require" to autorequire the series of "arb" gems.

--- lib/arb.rb
+++ lib/arb.rb
@@ -1,16 +1,13 @@
-require "arb/version"
-
 module Arb
-	class << self
-		attr_accessor :default_load		
-	end
-
-	#gems to be autoloaded
-	self.default_load=[:sqlite3]
-
-	Kernel.send :define_method,:arb_init do |*args,&block|
-		tmp=''
-		(Arb.default_load||[]).each { |item| tmp.tap{|x| p 'loading'} <<item.to_s<<',' if require item.to_s }
-		tmp.empty? ? 'nothing imported!': tmp.chop<<' imported!'
-	end
+  class << self
+    attr_accessor :required_gems
+  end
+  #param[0]: prefix of a gem
+  Kernel.send :define_method, :arb_require do |*args, &block|
+    Arb.required_gems||=[]
+    tmp=[]
+    $LOAD_PATH.each { |path| tmp<<Dir.tap { |dir| dir.chdir path }[(args[0] || 'arb_') +'*.rb'] }
+    tmp.flatten!.map! { |item| item[0..-4] }.each { |gem| Arb.required_gems<< gem if require gem }
+    Arb.required_gems.empty? ? 'Nothing required yet!' : 'Gems successfully required: ' << Arb.required_gems.uniq!.join(',')<<'!'
+  end
 end
