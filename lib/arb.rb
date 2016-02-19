ca0c96a83c514e91d1479412e0a3661857f83853
arybin <arybin@163.com>
2016-02-20 00:35:33 +0800

    fix some bugs

--- lib/arb.rb
+++ lib/arb.rb
@@ -1,16 +1,16 @@
 module Arb
-  class << self
-    attr_accessor :required_gems
-  end
-  #params: prefix of gems that need to be required 
-  Kernel.send :define_method, :arb_require do |*args, &block|
-    Arb.required_gems||=[]
-    args.unshift 'arb_'
-    tmp=[]
-    $LOAD_PATH.each { |path| args.each { |arg| tmp<<Dir.tap { |dir| dir.chdir path }[arg.to_s+'*.rb'] } }
-    tmp.flatten!.map! { |item| item[0..-4] }.each { |gem| Arb.required_gems<< gem if require gem }
-    Arb.required_gems.empty? ? 'Nothing required yet!' : 'Gems successfully required: ' << Arb.required_gems.uniq.join(',')<<'!'
-  end
+	class << self
+		attr_accessor :required_gems
+	end
+	#params: prefix of gems that need to be required 
+	Kernel.send :define_method, :arb_require do |*args, &block|
+		Arb.required_gems||=[]
+		args.unshift 'arb_'
+		tmp=[]
+		args.each { |arg| tmp << Gem::Specification.select { |gem| gem.name=~/^#{arg}/ }.map { |gem| gem.name }}	
+		tmp.compact.uniq.flatten!.each { |gem| Arb.required_gems<< gem if require gem }
+		Arb.required_gems.empty? ? 'Nothing required yet!' : 'Gems successfully required: ' << Arb.required_gems.uniq.join(',')<<'!'
+	end
 
-  arb_require
+	arb_require
 end
