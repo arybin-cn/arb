a1f5112ea332f0294ea3a210e4869f4ae105c94c
arybin <arybin@163.com>
2016-02-19 22:03:17 +0800

    fix some bugs

--- lib/arb.rb
+++ lib/arb.rb
@@ -2,12 +2,15 @@ module Arb
   class << self
     attr_accessor :required_gems
   end
-  #param[0]: prefix of a gem
+  #params: prefix of gems that need to be required 
   Kernel.send :define_method, :arb_require do |*args, &block|
     Arb.required_gems||=[]
+    args.unshift 'arb_'
     tmp=[]
-    $LOAD_PATH.each { |path| tmp<<Dir.tap { |dir| dir.chdir path }[(args[0] || 'arb_') +'*.rb'] }
+    $LOAD_PATH.each { |path| args.each { |arg| tmp<<Dir.tap { |dir| dir.chdir path }[arg.to_s+'*.rb'] } }
     tmp.flatten!.map! { |item| item[0..-4] }.each { |gem| Arb.required_gems<< gem if require gem }
-    Arb.required_gems.empty? ? 'Nothing required yet!' : 'Gems successfully required: ' << Arb.required_gems.uniq!.join(',')<<'!'
+    Arb.required_gems.empty? ? 'Nothing required yet!' : 'Gems successfully required: ' << Arb.required_gems.uniq.join(',')<<'!'
   end
+
+  arb_require
 end
