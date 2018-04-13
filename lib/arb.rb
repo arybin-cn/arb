8f1ae47ff96b2dbd737864497bd234889fa6251f
arybin <arybin@163.com>
2018-04-13 12:27:08 +0800

    refined

--- lib/arb.rb
+++ lib/arb.rb
@@ -8,7 +8,7 @@ module Kernel
       loop do 
         tmp=modules.shift
         break unless tmp
-        if require("arb/#{tmp}")
+        if require("arb/#{tmp.downcase}")
           imported<<tmp 
         else
           #warn("Fail to require arb/#{tmp}, maybe it had already been required!")
@@ -23,37 +23,31 @@ module Kernel
 
   def arb_use(arb_module_name,prefix=nil,forced=false)
     prefix="#{prefix}_" if prefix && !prefix.to_s.end_with?('_')
-    arb_module_name=arb_module_name.to_s.capitalize!
+    arb_module_name=arb_module_name.to_s.capitalize.to_sym
     res={:succeed=>[],:failed=>[]}
     tried_arb_use=false
-    begin
-      arb_module=Module.const_get(:Arb).const_get(arb_module_name)
-      Kernel.class_eval do
-        arb_module.singleton_methods.each do |m|
-          mm=prefix.to_s+m.to_s
-          if !forced && Kernel.instance_methods.include?(mm)
-            warn("Conflict method: #{mm}")
-            res[:failed] << mm
-            next
-          end
-          define_method(mm) do |*args,&block|
-            arb_module.send(m,*args,&block)
-          end
-          res[:succeed] << mm
+    #always try to import
+    arb_import arb_module_name
+    return nil unless Arb.constants.include?(arb_module_name)
+    arb_module=Module.const_get(:Arb).const_get(arb_module_name)
+    Kernel.class_eval do
+      arb_module.singleton_methods.each do |m|
+        mm=prefix.to_s+m.to_s
+        if !forced && Kernel.instance_methods.include?(mm)
+          warn("Conflict method: #{mm}")
+          res[:failed] << mm
+          next
         end
+        define_method(mm) do |*args,&block|
+          arb_module.send(m,*args,&block)
+        end
+        res[:succeed] << mm
       end
-      res
-    rescue NameError=>e
-      #Note that NoMethodError is subclass of NameError, which means that
-      #invoking nil.xxx(that throws NoMethodError) will also lead to the
-      #Name Error, we have to make sure that e is the instance of NameError!!!
-      if e.instance_of?(NameError) && !tried_arb_use
-        tried_arb_use=true
-        arb_import(arb_module_name.downcase)
-        retry
-      end
-      warn("Arb::#{arb_module_name} not found!")
     end
+    res
+    #Note that NoMethodError is subclass of NameError, which means that
+    #invoking nil.xxx(that throws NoMethodError) will also lead to the
+    #Name Error, we have to make sure that e is the instance of NameError!!!
   end
 
   alias_method :import,:arb_import
--- lib/arb/version.rb
+++ lib/arb/version.rb
@@ -1,3 +1,3 @@
 module Arb
-  VERSION = "1.2.7"
+  VERSION = "1.2.8"
 end
